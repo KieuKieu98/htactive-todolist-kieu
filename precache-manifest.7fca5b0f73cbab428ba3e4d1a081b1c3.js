5ce901fc5acce2b1e816fdd3a8871231ed78ff91
kieu.nguyen <kieu.nguyen@student.passerellesnumeriques.org>
2019-05-31 09:54:54 +0700

    Updates

---
+++ precache-manifest.7fca5b0f73cbab428ba3e4d1a081b1c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb809263f32d8a560b6a2f8d21cf950",
+    "url": "/KieuKieu98/htactive-todolist-kieu/index.html"
+  },
+  {
+    "revision": "4ec21abb678d299a6574",
+    "url": "/KieuKieu98/htactive-todolist-kieu/static/css/main.8426406d.chunk.css"
+  },
+  {
+    "revision": "9c6abeb9fdbb108ddb6f",
+    "url": "/KieuKieu98/htactive-todolist-kieu/static/js/2.2262bf60.chunk.js"
+  },
+  {
+    "revision": "4ec21abb678d299a6574",
+    "url": "/KieuKieu98/htactive-todolist-kieu/static/js/main.b90c1d53.chunk.js"
+  },
+  {
+    "revision": "d8a10e0cbae568f19307",
+    "url": "/KieuKieu98/htactive-todolist-kieu/static/js/runtime~main.f48b1e3d.js"
+  }
+]);
